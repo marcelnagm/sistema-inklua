b07d160f94d6488f200da9e76276de0d1846a905
marcel <marcel@marcel-550XDA>
2022-07-09 08:57:34 -0400

    ajuste  da data de nascimento

--- app/Models/CandidateHunting.php
+++ app/Models/CandidateHunting.php
@@ -214,7 +214,7 @@ class CandidateHunting extends Model {
         $data['state'] = $this->state() . '';
         $data['city'] = $this->city() . '';
         $data['payment'] = $this->payment_formatted() . '';
-        $data['bith_date'] = $this->birth_date->format('d/m/Y') . '';
+        $data['bith_date'] = $this->birth_date->format('d/m/Y') ;
         $data['pcd_type'] = $this->pcd_typo();
         $data['cellphone'] = $this->phone();
         $data['english_level'] = $this->english_level_obj() . '';
