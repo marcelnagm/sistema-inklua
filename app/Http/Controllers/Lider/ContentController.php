e29e2da1cb4edc14aa2e88c2b503ba8476fa4a25
marcel <marcel@marcel-550XDA>
2022-07-21 10:15:23 -0400

    ajuste para exibir mensagem de erro caso exista ajuste para formato convencionado por mim e rafael

--- app/Http/Controllers/Lider/ContentController.php
+++ app/Http/Controllers/Lider/ContentController.php
@@ -22,45 +22,44 @@ class ContentController extends Controller {
 
     public function replacement(Request $request, $id) {
         $reports = CandidateReport::whereIn('id', explode(',', $request->input('report_id')))->get();
-        $i =0;
-        $j= 0;
-        foreach ($reports as $report){
-            if($report->candidate()->status != -9999) $i++;
-            else {$report->replacement();
-            $j++;
+        $i = 0;
+        $j = 0;
+        foreach ($reports as $report) {
+            if ($report->candidate()->status != -9999)
+                $i++;
+            else {
+                $report->replacement();
+                $j++;
             }
         }
-        return array('status' => true,'message' => 'Selecionado '.$reports->count().' candidatos [reposto -'.$j.', ignorados - '.$i.' ]!');
+        return array('status' => true, 'error' => false, 'message' => 'Selecionado ' . $reports->count() . ' candidatos [reposto -' . $j . ', ignorados - ' . $i . ' ]!');
 //        dd($reports);
-        
     }
-    
+
     public function selects(Request $request, $id) {
         $reports = CandidateReport::whereIn('id', explode(',', $request->input('report_id')))->get();
-        foreach ($reports as $report){
+        foreach ($reports as $report) {
             $report->report_status_id = 9;
             $report->save();
         }
-        return array('status' => true,'message' => 'Selecionado '.$reports->count().' candidatos!');
+        return array('status' => true, 'error' => false, 'message' => 'Selecionado ' . $reports->count() . ' candidatos!');
 //        dd($reports);
-        
     }
-    
+
     public function sendClient(Request $request, $id) {
-        $reports = CandidateReport::where('report_status_id',9)
-                ->where('job_id',$id)
+        $reports = CandidateReport::where('report_status_id', 9)
+                ->where('job_id', $id)
                 ->get();
-        foreach ($reports as $report){
+        foreach ($reports as $report) {
             $report->report_status_id = 8;
             $report->save();
         }
-        return array('status' => true,'message' => 'Enviado '.$reports->count().' candidatos ao cliente!');
+        return array('status' => true, 'error' => false, 'message' => 'Enviado ' . $reports->count() . ' candidatos ao cliente!');
 //        dd($reports);
-        
     }
-     
+
     public function details(Request $request, $id) {
-        $content = Content::where('id', $id)->first();
+        $content = Content::findOrFail($id);
         $contentclient = $content->contentclient();
         $data = array();
         $data['titulo_vaga'] = $content->title;
@@ -77,38 +76,36 @@ class ContentController extends Controller {
         } else {
             $data['faturamento'] = '-';
         }
-         $data['c/o'] = $content->getLikesCount() . '/' . $data['vagas'];
-         $data['status'] = $content->getStatusFront();
-         $i=0;
-         $report = new \App\Models\CandidateReport;
-         foreach ($content->candidateReport() as $report){
-             $candidate = $report->candidate();
-             $data['candidate'][$i]['gid']=  $candidate->gid;
-             $data['candidate'][$i]['name']=  $candidate->full_name();
-             $data['candidate'][$i]['salary']=  $candidate->payment;
-             $data['candidate'][$i]['status']=  $report->reportstatus().'';
-             $data['candidate'][$i]['start_at']=  $report->start_at ? $report->start_at->format('d/m/Y') : '-';
-             $data['candidate'][$i]['owner']=  $report->owner_formatted();
-         }
-          
-        return $data;
+        $data['c/o'] = $content->getLikesCount() . '/' . $data['vagas'];
+        $data['status'] = $content->getStatusFront();
+        $i = 0;
+        $report = new \App\Models\CandidateReport;
+        foreach ($content->candidateReport() as $report) {
+            $candidate = $report->candidate();
+            $data['candidate'][$i]['gid'] = $candidate->gid;
+            $data['candidate'][$i]['name'] = $candidate->full_name();
+            $data['candidate'][$i]['salary'] = $candidate->payment;
+            $data['candidate'][$i]['status'] = $report->reportstatus() . '';
+            $data['candidate'][$i]['start_at'] = $report->start_at ? $report->start_at->format('d/m/Y') : '-';
+            $data['candidate'][$i]['owner'] = $report->owner_formatted();
+        }
+
+        return array('data' =>  $data);
     }
 
     public function description(Request $request) {
         
     }
-    
-    public function changeRecruiter(Request $request,$id ) {
-          $user = User::findOrFail($request->input('recruiter_id'));
-         $content = Content::findOrFail($id);
+
+    public function changeRecruiter(Request $request, $id) {
+        $user = User::findOrFail($request->input('recruiter_id'));
+        $content = Content::findOrFail($id);
         $contentclient = $content->contentclient();
         $content->user_id = $user->id;
         $contentclient->user_id = $user->id;
         $content->save();
         $contentclient->save();
-        return array('status' => true,'message' => 'Recrutador trocado com sucesso!');
+        return array('status' => true, 'error' => false, 'message' => 'Recrutador trocado com sucesso!');
     }
-    
-    
 
 }
