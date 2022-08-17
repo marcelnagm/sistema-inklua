25c2e511b03efd4e3f1269f5c644c5e09912dad8
marcel <marcel@marcel-550XDA>
2022-08-17 18:00:13 -0400

    Ajustes nas mensagens de erro

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,85 +9,90 @@ use App\Jobs\BoletoVerify;
 use Carbon\Carbon;
 use Symfony\Component\HttpKernel\Log\Logger;
 
-class TransactionController extends Controller
-{
-    public function __construct()
-    {
+class TransactionController extends Controller {
+
+    public function __construct() {
         $this->middleware('auth:api');
     }
 
-    public function create(Request $request)
-    {
+    public function create(Request $request) {
         $user = $request->user();
-        
+
         $transaction = Transaction::where('content_id', $request->input('content_id'))
                 ->where('status', 'paid');
-        
-        if($transaction->count() > 0)
-             return response()->json([
-                 'error' => true,
-                 'status' => false,
-                 "msg" => 'A vaga já se encontra paga'
-                 ]);
-        
-        $position = Content::where('id', $request->input('content_id'))
-                                ->where('type', 1)
-                                ->whereNotNull('user_id')                               
-                                ->with('user')
-                                ->first();
-                                
-        if($position == null){
+
+        if ($transaction->count() > 0)
             return response()->json([
-                 'error' => true,
-                 'status' => false,
-                "error" => 'Vaga não encontrada.']);
+                        'error' => true,
+                        'status' => false,
+                        "msg" => 'A vaga já se encontra paga'
+            ]);
+
+        $position = Content::where('id', $request->input('content_id'))
+                ->where('type', 1)
+                ->whereNotNull('user_id')
+                ->with('user')
+                ->first();
+
+        if ($position == null) {
+            return response()->json([
+                        'error' => true,
+                        'status' => false,
+                        "error" => 'Vaga não encontrada.']);
         }
-        
-         if($position->status != 'aguardando_pagamento'){
+
+        if ($position->status != 'aguardando_pagamento') {
 
             return response()->json([
-                "msg" => 'Vaga encontrada mas o status da vaga é: '.$position->status,
-                 'error' => true,
-                 'status' => false
-                ]);
+                        "msg" => 'Vaga encontrada mas o status da vaga é: ' . $position->status,
+                        'error' => true,
+                        'status' => false
+            ]);
         }
-        
+
 
         $transaction = Transaction::create([
-            'content_id' => $position->id,            
+                    'content_id' => $position->id,
         ]);
 
         try {
 
             $pagarme = json_decode($transaction->createOrder(Transaction::getCustomer($user), Transaction::getPayments()), true);
-            if(env('PAGARME_LOGGER'))
-            logger($pagarme);
-            if(!isset($pagarme["id"])){
-                return response()->json($pagarme);
+            if (env('PAGARME_LOGGER'))
+                logger($pagarme);
+            if (!isset($pagarme["id"])) {
+                return response()->json([
+                            'status' => false,
+                            'error' => true,
+                            'msg' => $pagarme]);
             }
             $transaction->updateFromGateway($pagarme);
 
-            if($transaction->status == 'paid') {
+            if ($transaction->status == 'paid') {
                 $position->update(['status' => 'publicada', 'published_at' => Carbon::now()->format('Y-m-d')]);
                 $position->notifyPositionPublished();
             }
-            if(env('PAGARME_LOGGER'))
-            logger($transaction);
-            return response()->json($transaction);
-
-        }catch(Exception $erros){
+            if (env('PAGARME_LOGGER'))
+                logger($transaction);
+            return response()->json([
+                            'status'  => false,
+                            'error'  => true,
+                            'msg'=> $transaction ]);
+        } catch (Exception $erros) {
             return response()->json(
-                [
-                    "code" => "MESSAGE_NOT_SENT",
-                    "errors" => $pagarme
-                ], 400
+                            [
+                                'status' => false,
+                                'error' => true,
+                                "code" => "MESSAGE_NOT_SENT",
+                                "errors" => $pagarme
+                            ]
             );
         }
-
     }
 
-    public function order(){
-         BoletoVerify::dispatch();
+    public function order() {
+        BoletoVerify::dispatch();
         return;
     }
+
 }
